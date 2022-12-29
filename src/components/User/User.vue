4d5b1ecb7a7980395f99f70e4d19374f89552e13
unknown <nh11b.l99@gmail.com>
2022-12-29 15:12:43 +0700

    add page button

--- src/components/User/User.vue
+++ src/components/User/User.vue
@@ -2,41 +2,28 @@
   <div>
     <h1>List user</h1>
     <Userlist :userlist="userlist" />
-    <button
-      @click="activeBtn = 'btn1'; handlePage(1)"
-      :class="{ active: activeBtn === 'btn1' }"
-    >
-      1
-    </button>
-    <button
-      @click="activeBtn = 'btn2'; handlePage(2)"
-      :class="{ active: activeBtn === 'btn2' }"
-    >
-      2
-    </button>
+    <UserPage :handlePage="handlePage" />
   </div>
 </template>
-
 <script>
 import Userlist from "./Uselist.vue";
+import UserPage from "./UserPage.vue";
 import axios from "axios";
 export default {
   name: "User",
   components: {
-    Userlist
+    Userlist,
+    UserPage
   },
   data() {
     return {
       userlist: [],
-      activeBtn: "btn1",
-      page: 2
     };
   },
-  mounted() {
-    let getUser = async page => {
+  created() {
+    let getUser = async () => {
       try {
-        let data = await axios.get(`https://reqres.in/api/users?page=${page}`);
-        console.log('check page ',page);
+        let data = await axios.get(`https://reqres.in/api/users?page=1`);  
         if (data.data.data) {
           this.userlist = data.data.data;
         }
@@ -47,8 +34,17 @@ export default {
     getUser();
   },
   methods: {
-    handlePage(p) {
-      this.page = p;
+    async handlePage(p) {
+      try {
+        console.log('check page ',p);
+        let data = await axios.get(`https://reqres.in/api/users?page=${p}`);
+        console.log('check data ',data)
+        if (data.data.data) {
+          this.userlist = data.data.data;
+        }
+      } catch (err) {
+        console.log("check error ", err);
+      }
     }
   }
 };
---
+++ src/components/User/UserPage.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button
+      @click="activeBtn = 1; handlePage(1)"
+      :class="{ active: activeBtn === 1 }"
+    >
+      1
+    </button>
+    <button
+      @click="activeBtn = 2; handlePage(2)"
+      :class="{ active: activeBtn === 2 }"
+    >
+      2
+    </button>
+    </div>
+</template>
+<script>
+export default{
+    data(){
+        return{
+            activeBtn: 1
+        }
+    },
+    methods:{
+        handleOnlick(page){
+            handlePage(page)
+        }
+    },
+    props:{
+        handlePage: Function
+    }
+}
+</script>
+<style></style>
